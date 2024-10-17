4ef26e358c7afff26d74b92d7261e374d46c45fe
Rajoooooo <rajocastro1211@gmail.com>
2024-10-17 23:24:32 +0800

    Updated Home Controller

--- angel-mvc/src/Controllers/HomeController.php
+++ angel-mvc/src/Controllers/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends BaseController
     {
         $template = 'home';
         $data = [
-            'student' => 'REPLACE THIS WITH YOUR FULL NAME',
+            'student' => 'RAFAEL JOHN L. CASTRO',
             'title' => 'IPT10 Laboratory Activity #9',
             'college' => 'College of Computer Studies',
             'university' => 'Angeles University Foundation',
